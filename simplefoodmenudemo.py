611d2381467ed25777c0787d1d8d981e29ec6ad7
mikramif <89080764+mikramif@users.noreply.github.com>
2021-10-02 16:18:00 +0800

    Update simplefoodmenudemo.py

--- simplefoodmenudemo.py
+++ simplefoodmenudemo.py
@@ -20,7 +20,7 @@ if readme:
 
     st.write("<a href='https://www.linkedin.com/in/yong-poh-yu/'>Dr. Yong Poh Yu </a>", unsafe_allow_html=True)
 
-st.write("Choose an option from the radio button on the side bar to continue.")
+st.write("Choose an option from the drop down menu on the side bar to continue.")
 
 
 option = st.sidebar.selectbox(
